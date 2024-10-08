ed18777aae572f62a66fa68f969b2b3310b2397d
unknown <mtgmirage@protonmail.com>
2024-10-08 15:44:44 -0500

    added vehicle model choice feature

--- scripts/Order.js
+++ scripts/Order.js
@@ -1,11 +1,14 @@
 export const OrdersList = async () => {
-    const fetchResponse = await fetch("http://localhost:8088/orders?_expand=technology&_expand=wheel&_expand=paint&_expand=interior")
+    const fetchResponse = await fetch("http://localhost:8088/orders?_expand=technology&_expand=wheel&_expand=paint&_expand=interior&_expand=variety")
     const orders = await fetchResponse.json()
 
     let ordersHTML = ""
     const ordersStringArray = orders.map(
         (order) => {
-            const orderPrice = order.wheel.price + order.interior.price + order.paint.price + order.technology.price
+            const subTotal = order.wheel.price + order.interior.price + order.paint.price + order.technology.price
+            const varietyModifier = order.variety.modifier
+            const varietyMultiplier = varietyModifier * .01
+            const orderPrice = subTotal * varietyMultiplier
 
             return `<section>
                 <div>
